99d9778dfcdfea5d94cb2b69886e0e79d28328e1
DerpyChap <derpychap@outlook.com>
2018-01-27 13:18:20 +0000

    Update for new package name

--- mattophobia_says/__init__.py
+++ mattophobia_says/__init__.py
@@ -11,7 +11,7 @@ __title__ = 'mattophobia_says'
 __author__ = 'DerpyChap'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 DerpyChap'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from collections import namedtuple
 from .mattsays import MattSays
@@ -19,4 +19,4 @@ from .mattsays import MattSays
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
 version_info = VersionInfo(
-    major=0, minor=1, micro=0, releaselevel='final', serial=0)
+    major=0, minor=1, micro=1, releaselevel='final', serial=0)
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='mattophobia_says',
     packages=['mattophobia_says'],
-    version='0.1',
+    version='0.1.1',
     description='A Mattophobia string generator',
     author='DerpyChap',
     author_email='derpychap@outlook.com',
-    url='https://github.com/DerpyChap/mattophobia-says',
+    url='https://github.com/DerpyChap/mattophobia_says',
     download_url=
-    'https://github.com/DerpyChap/mattophobia-says/archive/1.0.tar.gz',
+    'https://github.com/DerpyChap/mattophobia_says/archive/0.1.1.tar.gz',
     keywords=['mattophobia', 'sentences', 'swearing'],
     classifiers=[],
 )
